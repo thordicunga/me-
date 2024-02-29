f54cbe503982defffae5aed7ba7b9ac32161b3d5
thordicunga <161748464+thordicunga@users.noreply.github.com>
2024-02-29 23:50:53 +0700

    Add files via upload
    check var

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Mẹ Iu';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100019135912761';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Chúc mừng Ngày Phụ nữ 8/3,';
+const contentCard = ' mẹ yêu quý của con!Mẹ là nguồn động viên vô điều kiện và tình thương mãi mãi trong cuộc sống của con. Con biết ơn mẹ và yêu mẹ hơn bất cứ điều gì. Chúc mừng ngày của mẹ, người phụ nữ tuyệt vời nhất!Yêu mẹ! 🌹';
+
+
+const giftImage = 'nen-anh.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
